0ac8b99c4cf1b7fcbb092d51c2480f805537b27f
Victor Melo <126033285+V1ct0rM3lo@users.noreply.github.com>
2025-05-12 11:45:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -112,9 +112,9 @@ app.post('/atualizar-status/:codEmpresa', async (req, res) => {
 
 app.get('/pixel', (req, res) => {
     const email = req.query.email;
-    const ip = req.headers['x-forwarded-for'] || 'IP não encontrado'; // Sem usar req.connection
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-    console.log(`E-mail aberto por: ${email} - IP: ${ip} - ${new Date().toISOString()}`);
+    console.log(E-mail aberto por: ${email} - IP: ${ip} - ${new Date().toISOString()});
 
     const img = Buffer.from(
         'R0lGODlhAQABAPAAAAAAAAAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==',
